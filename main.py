6d364f5a165b9b465a4cb2c64b45f91bd4d59f3b
h1gf68 <gh0st@gh0.st>
2023-10-28 11:41:18 +0300

    add tests for algorithmKMP

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from algorithms.algorithm_kmp import AlgorithmKnuthMorrisPratt
 
 def main():
     text = "лилилось лилилась"
-    subtext = "и"
+    subtext = "лилила"
     alg_kmp = AlgorithmKnuthMorrisPratt(subtext, text)
 
     print(alg_kmp, type(alg_kmp))
--- tests/test_algorithm_kmp.py
+++ tests/test_algorithm_kmp.py
@@ -1,17 +1,71 @@
 from algorithms.algorithm_kmp import AlgorithmKnuthMorrisPratt
 
 
+obj = AlgorithmKnuthMorrisPratt("kikika", "kikilaki kikikata")
+
+
 def test_create_array():
-    obj = AlgorithmKnuthMorrisPratt("kikika", "kikilaki kikikata")
     assert obj.create_array() == [0, 0, 1, 2, 3, 0]
 
 
 def test_create_array_empty_subtext():
-    obj = AlgorithmKnuthMorrisPratt("", "kikilaki kikikata")
+    obj.subtext = ""
     assert obj.create_array() is None
 
 
 def test_create_array_one_sym_in_subtext():
-    obj = AlgorithmKnuthMorrisPratt("2", "kikilaki kikikata")
+    obj.subtext = "2"
     assert obj.create_array() == [0]
 
+
+def test_create_array_two_syms_in_subtext():
+    obj.subtext = "2a"
+    assert obj.create_array() == [0, 0]
+
+
+def test_create_array_two_equal_syms_in_subtext():
+    obj.subtext = "tt"
+    assert obj.create_array() == [0, 1]
+
+
+def test_search_subtext():
+    obj.subtext = "kikika"
+    obj.text = "kikilaki kikikata"
+    assert obj.search_subtext() == 9
+
+
+def test_search_subtext_empty_subtext():
+    obj.subtext = ""
+    obj.text = "kikilaki kikikata"
+    assert obj.search_subtext() is None
+
+
+
+def test_search_subtext_no_success_search():
+    obj.subtext = "bro"
+    obj.text = "kikilaki kikikata"
+    assert obj.search_subtext() is None
+
+
+def test_search_subtext_where_text_shorter_than_subtext():
+    obj.subtext = "kikika long subtext for test"
+    obj.text = "kikilaki kikikata"
+    assert obj.search_subtext() is None
+
+
+def test_search_subtext_where_subtext_equal_text():
+    obj.subtext = "kikilaki kikikata"
+    obj.text = "kikilaki kikikata"
+    assert obj.search_subtext() == 0
+
+
+def test_search_subtext_with_empty_text():
+    obj.subtext = "kikika"
+    obj.text = ""
+    assert obj.search_subtext() is None
+
+
+def test_search_subtext_with_empty_text_and_subtext():
+    obj.subtext = ""
+    obj.text = ""
+    assert obj.search_subtext() is None
