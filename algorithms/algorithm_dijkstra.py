a35e6219e947ed159e92964474d098804e317338
gh0st <gh0st@gh0.st>
2023-11-19 18:43:12 +0300

    changes

--- algorithms/algorithm_dijkstra.py
+++ algorithms/algorithm_dijkstra.py
@@ -1,6 +1,10 @@
 import math
 import heapq
 import queue
+import time
+import sys
+
+import memory_profiler
 
 
 def dijkstra(arr, n, s, f, m):
@@ -72,7 +76,7 @@ def dijkstra_path(arr, n, s, f, m):
     return " ".join(map(str, res[::-1])) if len(res) > 1 else -1
 
 
-def dfs(visited, curr_town, dist, goal_town):
+def dfs(graph, tv, visited, curr_town, dist, goal_town):
     visited[curr_town] = True
     for town_, dist_, t_ in graph[curr_town]:
         if not visited[town_]:
@@ -86,60 +90,64 @@ def dfs(visited, curr_town, dist, goal_town):
             else:
                 dfs(town_, dist_, goal_town)
 
-def dijkstra_sled_quick(graph, start):
+
+
+
+def dijkstra_sled_quick(graph, tv, start):
     l = [(0, start)]
     heapq.heapify(l)
     m = math.inf
-    time_ = [m for _ in range(n + 1)]
+    n = len(graph)
+    time_ = [m for _ in range(n)]
     time_[start] = 0
-    visited = [False for _ in range(n + 1)]
+    visited = [False for _ in range(n)]
     visited[0] = True
-    path = [-1 for _ in range(n + 1)]
+    path = [-1 for _ in range(n)]
 
     while l:
         t, town = heapq.heappop(l)
         if visited[town]:
             continue
-        for town_ in range(1, n+1):
-            dist_ = graph[town][town_]
-            t_ = tv[town][0] + dist_/tv[town][1]
+        for town_, dist_ in graph[town]:
+            t_ = tv[town_][0] + dist_ / tv[town_][1]
 
             if visited[town_]:
                 continue
+
             t_ += time_[town]
             if time_[town_] > t_:
                 time_[town_] = t_
                 heapq.heappush(l, (time_[town_], town_))
                 path[town_] = town
         visited[town] = True
-    # if start in (2,3,4,16, 27, 36): print(start, time_)
-    return time_[1], path
+
+    return time_, path
 
 
-def bfs(graph, town_dist):
+def bfs(graph, graph_firstly, town_dist):
     q = queue.Queue()
     q.put(town_dist)
-    visited = [False for _ in range(n + 1)]
+
+    visited = [False for _ in range(len(graph))]
     visited[0] = True
     goal_town = town_dist[0]
 
     while not q.empty():
         town, dist = q.get()
-        print(town, dist)
-        if visited[town]:
-            continue
         visited[town] = True
+        for town_, dist_ in graph_firstly[town]:
+            if not visited[town_]:
+                d = dist + dist_
+                q.put((town_, d))
 
-        for town_, dist_ in graph[town][:]:
-            if visited[town_]:
-                continue
-            d = dist + dist_
-            graph[goal_town].append((town_, d))
-            q.put((town_, d))
+                if goal_town != town_:
+                    if goal_town > town_:
+                        goal_town, town_ = town_, goal_town
+                    graph[goal_town].append((town_, d))
     return graph
 
-
-if __name__ == "__main__":
+# @memory_profiler.profile
+def main():
     n = int(input().strip())
 
     tv = [() for _ in range(n + 1)]
@@ -148,23 +156,39 @@ if __name__ == "__main__":
         tv[i] = (t, v)
 
     graph = [[] for _ in range(n + 1)]
+    graph_firstly = [[] for _ in range(n+1)]
+
     for _ in range(1, n):
         a, b, s = map(int, input().strip().split())
-        graph[a].append((b, s))
-        graph[b].append((a, s))
+        if a > b:
+            a, b = b, a
+        # graph[a].append((b, s))
+        graph_firstly[a].append((b, s))
+
+    # sec = time.time()
 
     for i in range(1, n + 1):
-        graph = bfs(graph, (i, 0))
+        graph = bfs(graph, graph_firstly, (i, 0))
     for i in range(len(graph)):
-        print(i, "--", graph[i])
+        print(i, graph[i])
 
-    time_, path_ = max([dijkstra_sled_quick(graph, town) for town in range(2, n + 1)])
+    time_, path_ = dijkstra_sled_quick(graph, tv, 1)
+    ind = t = 0
+    for i in range(1, len(time_)):
+        if t < time_[i]:
+            t = time_[i]
+            ind = i
 
-    finish = 1
-    path = [finish]
-    while path_[finish] != -1:
-        path.append(path_[finish])
-        finish = path_[finish]
+    path = [ind]
+    while path_[ind] != -1:
+        path.append(path_[ind])
+        ind = path_[ind]
 
-    print('{:.10f}'.format(time_))
-    print(" ".join(map(str, path[::-1])))
+    print('{:.10f}'.format(t))
+    print(" ".join(map(str, path)))
+    # sec = time.time() - sec
+    # print(sec)
+
+
+if __name__ == "__main__":
+    main()
